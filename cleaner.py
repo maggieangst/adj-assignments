43f7b607fe9c6f5026d108313b7f4fc6f66108ff
Maggie Angst <maggieangst@Maggies-MacBook-Pro-6.local>
2016-03-02 20:57:58 -0600

    Add cleaner file

---
+++ cleaner.py
@@ -0,0 +1,18 @@
+# Your code goes here
+import csv
+
+csvfile = open('./data/cleanme.csv', 'r')
+outfile = open('./data/cleanme-clean.csv', 'w')
+ 
+reader = csv.DictReader(csvfile)
+writer = csv.DictWriter(outfile, reader.fieldnames)
+
+writer.writeheader()
+
+for row in reader:
+    row['first_name'] = row['first_name'].upper()
+    row['city'] = row['city'].replace('&nbsp;','')
+    row['zip'] = row['zip'].zfill(5)
+    if float(row['amount']) > 1000.0:
+        print row
+        writer.writerow(row)
